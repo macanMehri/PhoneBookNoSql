0e69524b0cb8fd9997b85a32558b788158e61551
Macan Mehri <macanmehri@gmail.com>
2024-03-07 13:25:28 +0330

    New constant file and added MENU to it

---
+++ constants.py
@@ -0,0 +1,28 @@
+FIRST_NAMES = [
+    'Macan', 'Hadi', 'Amir', 'Hassan', 'Hossein', 'Mehran', 'Mehrdad', 'Soroush',
+    'Hassan', 'Mina', 'Mehrane', 'Samira', 'Negin', 'Hadis', 'Arman', 'Ahmad', 'Reza',
+    'Hamid', 'Mohammad', 'Mohammadreza', 'Ali', 'Mahmod', 'Negar', 'Sarina', 'Armita'
+]
+
+LAST_NAMES = [
+    'Mehri', 'Khani', 'Amiri', 'Hassani', 'Hosseini', 'Mehrani', 'Mehrdadi', 'Soroushi',
+    'Hassani', 'Minai', 'Mehranei', 'Samirai', 'Negini', 'Hadisi', 'Armani', 'Ahmadi', 'Rezai',
+    'Hamidi', 'Mohammadi', 'Mohammadrezai', 'Asadi', 'Mahmodi', 'Negari', 'Sarinai', 'Armitai'
+]
+
+CITIES = [
+        'Tehran', 'Gorgan', 'Ahvaz', 'Sari', 'Mashhad', 'Joybar', 'Esfahan', 'Shiraz', 'Rasht'
+        'Karaj', 'Tabriz', 'Bojnord', 'Gonbad', 'Galogah', 'Sirjan', 'Sanandaj', 'kashan'
+]
+
+PROVICES = [
+        'Tehran', 'Esfahan', 'Fars', 'Mazandaran', 'Golestan', 'Khorasan Shomali',
+        'Khorasan Razavi', 'Khorasan Jonobi', 'Gilan', 
+]
+
+MENU = '''
+0. Exit.
+1. Show all datas.
+2. Add random datas to database.
+3. Add new data.
+'''
